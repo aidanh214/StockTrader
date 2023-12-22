496ec97a75b3dcb12ec8d1db1433c4ce6ce3ef0f
aidanh214 <hastings.aidan@gmail.com>
2023-12-23 00:57:54 +0100

    Finished up database classes. Still some last checks to be done - make sure database filetypes are always .db, some edge case testing.
    Next up, GUI!

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import requests
-import csv
-import tkinter as tk
-import datetime
-from tkinter import ttk
+import sqlite3
 
 # AlphaVantage API Key:
 # W2RO7B9H5RESE4N1
@@ -12,15 +8,15 @@ class SpendingData:
     def __init__(self):
         self.transactions = {}
 
-    def add_transaction(self, date, value, note):
-        if (date, note) in self.transactions:
-            self.transactions[date, note][2] += value
+    def add_transaction(self, trans_date, value, note):
+        if (trans_date, note) in self.transactions:
+            self.transactions[trans_date, note][2] += value
         else:
-            self.transactions[date, note] = [date, note, value]
+            self.transactions[trans_date, note] = [trans_date, note, value]
 
-    def remove_transaction(self, date, note):
-        if (date, note) in self.transactions:
-            del self.transactions[date,note]
+    def remove_transaction(self, trans_date, note):
+        if (trans_date, note) in self.transactions:
+            del self.transactions[trans_date, note]
         else:
             raise ValueError("No transaction to delete.")
 
@@ -30,34 +26,74 @@ class SpendingData:
         else:
             raise ValueError("No transaction to modify.")
 
-    def save_spending(self, user, password):
-        pass
+    def save_data(self, filename):
+        spend_string = ""
+        for spend in self.transactions:
+            spend_list = self.transactions[spend]
+            spend_string += ("('"+str(spend_list[0])+"', '"+str(spend_list[1])+"', "+str(spend_list[2])+"),"+" ")
+        spend_string = spend_string.removesuffix(", ")
+        con = sqlite3.connect(filename)
+        cur = con.cursor()
+        try:
+            cur.execute("DELETE FROM spending")
+            cur.execute("""INSERT INTO spending (date, note, value) VALUES"""+spend_string)
+        except sqlite3.OperationalError:
+            cur.execute("CREATE TABLE spending(date, note, value)")
+            cur.execute("DELETE FROM spending")
+            cur.execute("""INSERT INTO spending (date, note, value) VALUES """ + spend_string)
+        con.commit()
 
-    def load_spending(self, user, password):
-        pass
+    def load_data(self, filename):
+        con = sqlite3.connect(filename)
+        cur = con.cursor()
+        spends = cur.execute("SELECT date, note, value FROM spending ORDER BY date")
+        for row in spends:
+            date = row[0]
+            note = row[1]
+            value = int(row[2])
+            self.transactions[date, note] = [date, note, value]
 
 
 class InvestmentData:
     def __init__(self):
         self.portfolio = {}
 
-    def add_asset(self, asset_type):
-        if asset_type == "stock":
-            pass
-        elif asset_type == "bond":
-            pass
-        elif asset_type == "cash":
-            pass
-        elif asset_type == "other":
-            pass
+    def add_asset(self, asset_type, number_of_asset, note=""):
+        if (asset_type, note) in self.portfolio:
+            self.portfolio[asset_type, note] += number_of_asset
         else:
-            raise ValueError(str("No asset of type: " + asset_type))
+            self.portfolio[asset_type, note] = number_of_asset
 
-    def save_spending(self, user, password):
-        pass
+    def get_asset_value(self, asset_type, note=""):
+        asset_value = self.portfolio[asset_type, note]
+        return asset_value
 
-    def load_spending(self, user, password):
-        pass
+    def save_data(self, filename):
+        sql_string = ""
+        for asset in self.portfolio:
+            sql_string += "("
+            sql_string += "'" + asset[0] + "', "
+            sql_string += "'" + asset[1] + "', "
+            sql_string += "'" + str(self.portfolio[asset]) + "'"
+            sql_string += "),"
+        sql_string = sql_string.removesuffix(",")
+        con = sqlite3.connect(filename)
+        cur = con.cursor()
+        try:
+            cur.execute("DELETE FROM assets")
+            cur.execute("""INSERT INTO assets VALUES """ + sql_string)
+        except sqlite3.OperationalError:
+            cur.execute("CREATE TABLE assets(type, note, amount)")
+            cur.execute("DELETE FROM assets")
+            cur.execute("""INSERT INTO assets VALUES """ + sql_string)
+        con.commit()
+
+    def load_data(self, filename):
+        con = sqlite3.connect(filename)
+        cur = con.cursor()
+        assets = cur.execute("SELECT type, note, amount FROM assets ORDER BY type")
+        for row in assets:
+            self.portfolio[row[0],row[1]] = int(row[2])
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from main import *
 import unittest
+import tempfile
+import sqlite3
+import os
+from unittest.mock import patch, MagicMock
 
 
 class TestSpendingData(unittest.TestCase):
@@ -35,6 +39,111 @@ class TestSpendingData(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.spending_data.modify_transaction("2023-01-02", "Coffee", 50)
 
+    def test_save_data(self):
+        # Add some transactions for testing
+        self.spending_data.add_transaction("2023-01-01", 100, "Groceries")
+        self.spending_data.add_transaction("2023-01-02", 50, "Dining")
+        self.spending_data.add_transaction("2023-01-03", 30, "Shopping")
+
+        # Create a temporary database file for testing
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_filename = temp_file.name
+
+            # Save data
+            self.spending_data.save_data(temp_filename)
+
+            # Check if the data is saved correctly
+            con = sqlite3.connect(temp_filename)
+            cur = con.cursor()
+            saved_data = cur.execute("SELECT * FROM spending").fetchall()
+
+            expected_data = [
+                ('2023-01-01', 'Groceries', 100),
+                ('2023-01-02', 'Dining', 50),
+                ('2023-01-03', 'Shopping', 30)
+            ]
+
+            self.assertEqual(saved_data, expected_data)
+
+            # Clean up the temporary file
+            temp_file.close()
+            os.remove(temp_filename)
+
+    def test_load_data(self):
+        # Set up a known state of the data
+        self.spending_data.add_transaction("2023-01-01", 100, "Groceries")
+        self.spending_data.add_transaction("2023-01-02", 50, "Dining")
+        self.spending_data.add_transaction("2023-01-03", 30, "Shopping")
+
+        # Create a temporary database file for testing
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_filename = temp_file.name
+
+            # Save data
+            self.spending_data.save_data(temp_filename)
+
+            # Clear the existing data in the SpendingData instance
+            self.spending_data = SpendingData()
+
+            # Load data
+            self.spending_data.load_data(temp_filename)
+
+            # Check if the loaded data matches the expected state
+            expected_data = {
+                ('2023-01-01', 'Groceries'): ['2023-01-01', 'Groceries', 100],
+                ('2023-01-02', 'Dining'): ['2023-01-02', 'Dining', 50],
+                ('2023-01-03', 'Shopping'): ['2023-01-03', 'Shopping', 30]
+            }
+
+            self.assertEqual(self.spending_data.transactions, expected_data)
+
+            # Clean up the temporary file
+            temp_file.close()
+            os.remove(temp_filename)
+class TestInvestmentData(unittest.TestCase):
+    def setUp(self):
+        self.investment_data = InvestmentData()
+
+    def tearDown(self):
+        pass
+
+    @patch('sqlite3.connect')
+    def test_add_asset(self, mock_connect):
+        # Test adding stock
+        self.investment_data.add_asset("stock", 10, "AAPL")
+        self.assertEqual(self.investment_data.portfolio, {("stock", "AAPL"): 10})
+
+        # Test adding bond
+        self.investment_data.add_asset("bond", 5, "Government")
+        self.assertEqual(self.investment_data.portfolio, {("stock", "AAPL"): 10, ("bond", "Government"): 5})
+
+        # Test adding cash
+        self.investment_data.add_asset("cash", 1000)
+        self.assertEqual(self.investment_data.portfolio, {("stock", "AAPL"): 10, ("bond", "Government"): 5, ("cash", ""): 1000})
+
+    def test_save_load_data(self):
+        # Set up a mock portfolio
+        self.investment_data.portfolio = {("stock", "AAPL"): 10, ("bond", "Government"): 5, ("cash", ""): 1000}
+
+        # Create a temporary database file for testing
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_filename = temp_file.name
+
+            # Save data
+            self.investment_data.save_data(temp_filename)
+
+            # Create a new InvestmentData instance and load data from the file
+            new_investment_data = InvestmentData()
+            new_investment_data.load_data(temp_filename)
+
+            # Check if the loaded data matches the original data
+            self.assertEqual(self.investment_data.portfolio, new_investment_data.portfolio)
+
+            # Clean up the temporary file
+            temp_file.close()
+            os.remove(temp_filename)
 
 if __name__ == '__main__':
     unittest.main()
+
+
